c13e0446cc7e3de577cc6a51ddaf7531dd7553ec
Fritz Lin <uxfritz@163.com>
2024-01-02 16:23:41 +0800

    initial translation by Bing AI

--- elizadata_zh-cn.js
+++ elizadata_zh-cn.js
@@ -3,51 +3,52 @@
 // entries prestructured as layed out in Weizenbaum's description
 // [cf: Communications of the ACM, Vol. 9, #1 (January 1966): p 36-45.]
 
+// ** Credits: Translation done by Bing AI
 "use strict";
 
 exports.elizaInitials = [
-	"How do you do.  Please tell me your problem.",
+	"你好。请告诉我你的问题。",
 // additions (not original)
-	"Please tell me what's been bothering you.",
-	"Is something troubling you ?"
+	"请告诉我什么事情困扰着你。",
+	"有什么事情困扰你吗？"
 ];
 
 exports.elizaFinals = [
-	"Goodbye.  It was nice talking to you.",
+	"再见。很高兴和你交谈。",
 // additions (not original)
-	"Goodbye.  This was really a nice talk.",
-	"Goodbye.  I'm looking forward to our next session.",
-	"This was a good session, wasn't it -- but time is over now.   Goodbye.",
-	"Maybe we could discuss this moreover in our next session ?   Goodbye."
+	"再见。这次谈话真的很愉快。",
+	"再见。我期待我们的下一次会话。",
+	"这是一次好的会话，不是吗 -- 但是时间已经结束了。再见。",
+	"也许我们可以在下一次会话中更多地讨论这个问题？再见。"
 ];
 
 exports.elizaQuits = [
-	"bye",
-	"goodbye",
-	"done",
-	"exit",
-	"quit"
+	"再见",
+	"再见",
+	"完成",
+	"退出",
+	"退出"
 ];
 
 exports.elizaPres = [
-	"dont", "don't",
-	"cant", "can't",
-	"wont", "won't",
-	"recollect", "remember",
-	"recall", "remember",
-	"dreamt", "dreamed",
-	"dreams", "dream",
-	"maybe", "perhaps",
-	"certainly", "yes",
-	"machine", "computer",
-	"machines", "computer",
-	"computers", "computer",
-	"were", "was",
-	"you're", "you are",
-	"i'm", "i am",
-	"same", "alike",
-	"identical", "alike",
-	"equivalent", "alike"
+	"dont", "不",
+	"cant", "不能",
+	"wont", "不会",
+	"recollect", "记住",
+	"recall", "记住",
+	"dreamt", "梦想",
+	"dreams", "梦想",
+	"maybe", "也许",
+	"certainly", "是的",
+	"machine", "计算机",
+	"machines", "计算机",
+	"computers", "计算机",
+	"were", "是",
+	"you're", "你是",
+	"i'm", "我是",
+	"same", "相同",
+	"identical", "相同",
+	"equivalent", "相同"
 ];
 
 exports.elizaPosts = [
@@ -59,7 +60,7 @@ exports.elizaPosts = [
 	"i", "you",
 	"you", "I",
 	"my", "your",
-	"i'm", "you are"
+	"i'm", "你是"
 ];
 
 exports.elizaSynons = {
@@ -93,21 +94,21 @@ exports.elizaKeywords = [
 
 	["xnone", 0, [
 		["*", [
-			"I'm not sure I understand you fully.",
-			"Please go on.",
-			"What does that suggest to you ?",
-			"Do you feel strongly about discussing such things ?",
-			"That is interesting.  Please continue.",
-			"Tell me more about that.",
-			"Does talking about this bother you ?"
+			"我不确定我完全理解你。",
+			"请继续。",
+			"那对你有什么启示？",
+			"你强烈地讨论这些事情吗？",
+			"那很有趣。请继续。",
+			"告诉我更多关于那个的信息。",
+			"谈论这个会让你烦恼吗？"
 		]]
 	]],
 	["sorry", 0, [
 		["*", [
-			"Please don't apologise.",
-			"Apologies are not necessary.",
-			"I've told you that apologies are not required.",
-			"It did not bother me.  Please continue."
+			"请不要道歉。",
+			"不需要道歉。",
+			"我已经告诉过你，不需要道歉。",
+			"那并没有打扰我。请继续。"
 		]]
 	]],
 	["apologise", 0, [
@@ -117,274 +118,274 @@ exports.elizaKeywords = [
 	]],
 	["remember", 5, [
 		["* i remember *", [
-			"Do you often think of (2) ?",
-			"Does thinking of (2) bring anything else to mind ?",
-			"What else do you recollect ?",
-			"Why do you remember (2) just now ?",
-			"What in the present situation reminds you of (2) ?",
-			"What is the connection between me and (2) ?",
-			"What else does (2) remind you of ?"
+			"你经常想起(2)吗？",
+			"想起(2)还会想起什么吗？",
+			"你还记得什么？",
+			"为什么你现在记得(2)？",
+			"现在的情况让你想起了(2)吗？",
+			"我和(2)之间有什么联系？",
+			"(2)还让你想起了什么？"
 		]],
 		["* do you remember *", [
-			"Did you think I would forget (2) ?",
-			"Why do you think I should recall (2) now ?",
-			"What about (2) ?",
+			"你认为我会忘记(2)吗？",
+			"你为什么认为我现在应该记得(2)？",
+			"关于(2)呢？",
 			"goto what",
-			"You mentioned (2) ?"
+			"你提到了(2)？"
 		]],
 		["* you remember *", [
-			"How could I forget (2) ?",
-			"What about (2) should I remember ?",
+			"我怎么可能忘记(2)呢？",
+			"我应该记住(2)的什么呢？",
 			"goto you"
 		]]
 	]],
 	["forget", 5, [
 		["* i forget *", [
-			"Can you think of why you might forget (2) ?",
-			"Why can't you remember (2) ?",
-			"How often do you think of (2) ?",
-			"Does it bother you to forget that ?",
-			"Could it be a mental block ?",
-			"Are you generally forgetful ?",
-			"Do you think you are suppressing (2) ?"
+			"你能想出你为什么可能会忘记(2)吗？",
+			"为什么你不能记住(2)？",
+			"你经常想起(2)吗？",
+			"忘记那个会让你烦恼吗？",
+			"可能是心理障碍吗？",
+			"你总是健忘吗？",
+			"你认为你在压抑(2)吗？"
 		]],
 		["* did you forget *", [
-			"Why do you ask ?",
-			"Are you sure you told me ?",
-			"Would it bother you if I forgot (2) ?",
-			"Why should I recall (2) just now ?",
+			"你为什么问？",
+			"你确定你告诉过我吗？",
+			"如果我忘记了(2)，你会烦恼吗？",
+			"为什么我现在应该记得(2)？",
 			"goto what",
-			"Tell me more about (2)."
+			"告诉我更多关于(2)的信息。"
 		]]
 	]],
 	["if", 3, [
 		["* if *", [
-			"Do you think it's likely that (2) ?",
-			"Do you wish that (2) ?",
-			"What do you know about (2) ?",
-			"Really, if (2) ?",
-			"What would you do if (2) ?",
-			"But what are the chances that (2) ?",
-			"What does this speculation lead to ?"
+			"你认为(2)可能吗？",
+			"你希望(2)吗？",
+			"你对(2)了解多少？",
+			"真的，如果(2)？",
+			"如果(2)，你会怎么做？",
+			"但是(2)的可能性有多大？",
+			"这种推测会导致什么？"
 		]]
 	]],
 	["dreamed", 4, [
 		["* i dreamed *", [
-			"Really, (2) ?",
-			"Have you ever fantasized (2) while you were awake ?",
-			"Have you ever dreamed (2) before ?",
+			"真的，(2)？",
+			"你醒着的时候有没有幻想过(2)？",
+			"你以前梦见过(2)吗？",
 			"goto dream"
 		]]
 	]],
 	["dream", 3, [
 		["*", [
-			"What does that dream suggest to you ?",
-			"Do you dream often ?",
-			"What persons appear in your dreams ?",
-			"Do you believe that dreams have something to do with your problem ?"
+			"那个梦对你有什么启示？",
+			"你经常做梦吗？",
+			"什么人出现在你的梦里？",
+			"你认为梦和你的问题有关吗？"
 		]]
 	]],
 	["perhaps", 0, [
 		["*", [
-			"You don't seem quite certain.",
-			"Why the uncertain tone ?",
-			"Can't you be more positive ?",
-			"You aren't sure ?",
-			"Don't you know ?",
-			"How likely, would you estimate ?"
+			"你似乎不太确定。",
+			"为什么语气不确定？",
+			"你不能更肯定一点吗？",
+			"你不确定吗？",
+			"你不知道吗？",
+			"你估计有多大可能性？"
 		]]
 	]],
 	["name", 15, [
 		["*", [
-			"I am not interested in names.",
-			"I've told you before, I don't care about names -- please continue."
+			"我对名字不感兴趣。",
+			"我以前告诉过你，我对名字不感兴趣 -- 请继续。"
 		]]
 	]],
 	["deutsch", 0, [
 		["*", [
 			"goto xforeign",
-			"I told you before, I don't understand German."
+			"我之前告诉过你，我不懂德语。"
 		]]
 	]],
 	["francais", 0, [
 		["*", [
 			"goto xforeign",
-			"I told you before, I don't understand French."
+			"我之前告诉过你，我不懂法语。"
 		]]
 	]],
 	["italiano", 0, [
 		["*", [
 			"goto xforeign",
-			"I told you before, I don't understand Italian."
+			"我之前告诉过你，我不懂意大利语。"
 		]]
 	]],
 	["espanol", 0, [
 		["*", [
 			"goto xforeign",
-			"I told you before, I don't understand Spanish."
+			"我之前告诉过你，我不懂西班牙语。"
 		]]
 	]],
 	["xforeign", 0, [
 		["*", [
-			"I speak only English."
+			"我只会说英语。"
 		]]
 	]],
 	["hello", 0, [
 		["*", [
-			"How do you do.  Please state your problem.",
-			"Hi.  What seems to be your problem ?"
+			"你好。请告诉我你的问题。",
+			"嗨。你遇到什么问题了？"
 		]]
 	]],
 	["computer", 50, [
 		["*", [
-			"Do computers worry you ?",
-			"Why do you mention computers ?",
-			"What do you think machines have to do with your problem ?",
-			"Don't you think computers can help people ?",
-			"What about machines worries you ?",
-			"What do you think about machines ?",
-			"You don't think I am a computer program, do you ?"
+			"电脑让你感到困扰吗？",
+			"你为什么提到电脑？",
+			"你认为机器与你的问题有什么关系？",
+			"你不认为电脑可以帮助人们吗？",
+			"关于机器，有什么让你担忧的？",
+			"你对机器有什么看法？",
+			"你不会认为我是一个电脑程序，对吗？"
 		]]
 	]],
 	["am", 0, [
 		["* am i *", [
-			"Do you believe you are (2) ?",
-			"Would you want to be (2) ?",
-			"Do you wish I would tell you you are (2) ?",
-			"What would it mean if you were (2) ?",
+			"你相信你是(2)吗？",
+			"你想成为(2)吗？",
+			"你希望我告诉你你是(2)吗？",
+			"如果你是(2)，那会意味着什么？",
 			"goto what"
 		]],
 		["* i am *", [
 			"goto i"
 		]],
 		["*", [
-			"Why do you say 'am' ?",
-			"I don't understand that."
+			"你为什么说 'am'？",
+			"我不明白。"
 		]]
 	]],
 	["are", 0, [
 		["* are you *", [
-			"Why are you interested in whether I am (2) or not ?",
-			"Would you prefer if I weren't (2) ?",
-			"Perhaps I am (2) in your fantasies.",
-			"Do you sometimes think I am (2) ?",
+			"你为什么对我是否是(2)感兴趣？",
+			"你希望我不是(2)吗？",
+			"也许在你的幻想中，我是(2)。",
+			"你有时候认为我是(2)吗？",
 			"goto what",
-			"Would it matter to you ?",
-			"What if I were (2) ?"
+			"这对你有关系吗？",
+			"如果我是(2)，那又如何？"
 		]],
 		["* you are *", [
 			"goto you"
 		]],
 		["* are *", [
-			"Did you think they might not be (2) ?",
-			"Would you like it if they were not (2) ?",
-			"What if they were not (2) ?",
-			"Are they always (2) ?",
-			"Possibly they are (2).",
-			"Are you positive they are (2) ?"
+			"你认为他们可能不是(2)吗？",
+			"你希望他们不是(2)吗？",
+			"如果他们不是(2)，那又如何？",
+			"他们总是(2)吗？",
+			"可能他们是(2)。",
+			"你确定他们是(2)吗？"
 		]]
 	]],
 	["your", 0, [
 		["* your *", [
-			"Why are you concerned over my (2) ?",
-			"What about your own (2) ?",
-			"Are you worried about someone else's (2) ?",
-			"Really, my (2) ?",
-			"What makes you think of my (2) ?",
-			"Do you want my (2) ?"
+			"你为什么关心我的(2)？",
+			"你自己的(2)呢？",
+			"你担心别人的(2)吗？",
+			"真的，我的(2)？",
+			"是什么让你想到我的(2)？",
+			"你想要我的(2)吗？"
 		]]
 	]],
 	["was", 2, [
 		["* was i *", [
-			"What if you were (2) ?",
-			"Do you think you were (2) ?",
-			"Were you (2) ?",
-			"What would it mean if you were (2) ?",
-			"What does ' (2) ' suggest to you ?",
+			"如果你是(2)，那又如何？",
+			"你认为你是(2)吗？",
+			"你是(2)吗？",
+			"如果你是(2)，那会意味着什么？",
+			"' (2) '对你来说意味着什么？",
 			"goto what"
 		]],
 		["* i was *", [
-			"Were you really ?",
-			"Why do you tell me you were (2) now ?",
-			"Perhaps I already know you were (2)."
+			"你真的是吗？",
+			"你为什么现在告诉我你是(2)？",
+			"也许我已经知道你是(2)了。"
 		]],
 		["* was you *", [
-			"Would you like to believe I was (2) ?",
-			"What suggests that I was (2) ?",
-			"What do you think ?",
-			"Perhaps I was (2).",
-			"What if I had been (2) ?"
+			"你希望相信我是(2)吗？",
+			"有什么暗示我是(2)的？",
+			"你怎么看？",
+			"也许我是(2)。",
+			"如果我曾经是(2)，那又如何？"
 		]]
 	]],
 	["i", 0, [
 		["* i @desire *", [
-			"What would it mean to you if you got (3) ?",
-			"Why do you want (3) ?",
-			"Suppose you got (3) soon.",
-			"What if you never got (3) ?",
-			"What would getting (3) mean to you ?",
-			"What does wanting (3) have to do with this discussion ?"
+			"如果你得到(3)，对你意味着什么？",
+			"你为什么想要(3)？",
+			"假设你很快就能得到(3)。",
+			"如果你永远得不到(3)，那又如何？",
+			"得到(3)对你意味着什么？",
+			"想要(3)与这次讨论有什么关系？"
 		]],
 		["* i am* @sad *", [
-			"I am sorry to hear that you are (3).",
-			"Do you think coming here will help you not to be (3) ?",
-			"I'm sure it's not pleasant to be (3).",
-			"Can you explain what made you (3) ?"
+			"听到你是(3)，我很遗憾。",
+			"你认为来这里会帮助你不再是(3)吗？",
+			"我相信成为(3)一定不愉快。",
+			"你能解释一下是什么让你变成(3)的吗？"
 		]],
 		["* i am* @happy *", [
-			"How have I helped you to be (3) ?",
-			"Has your treatment made you (3) ?",
-			"What makes you (3) just now ?",
-			"Can you explain why you are suddenly (3) ?"
+			"我是如何帮助你变得(3)的？",
+			"你的治疗让你变得(3)了吗？",
+			"现在是什么让你(3)？",
+			"你能解释一下为什么你突然变得(3)吗？"
 		]],
 		["* i was *", [
 			"goto was"
 		]],
 		["* i @belief i *", [
-			"Do you really think so ?",
-			"But you are not sure you (3).",
-			"Do you really doubt you (3) ?"
+			"你真的这么认为吗？",
+			"但你不确定你是(3)。",
+			"你真的怀疑你是(3)吗？"
 		]],
 		["* i* @belief *you *", [
 			"goto you"
 		]],
 		["* i am *", [
-			"Is it because you are (2) that you came to me ?",
-			"How long have you been (2) ?",
-			"Do you believe it is normal to be (2) ?",
-			"Do you enjoy being (2) ?",
-			"Do you know anyone else who is (2) ?"
+			"是因为你是(2)，你才来找我吗？",
+			"你是(2)多久了？",
+			"你认为成为(2)是正常的吗？",
+			"你喜欢成为(2)吗？",
+			"你知道还有谁是(2)吗？"
 		]],
 		["* i @cannot *", [
-			"How do you know that you can't (3) ?",
-			"Have you tried ?",
-			"Perhaps you could (3) now.",
-			"Do you really want to be able to (3) ?",
-			"What if you could (3) ?"
+			"你怎么知道你不能(3)？",
+			"你试过吗？",
+			"也许你现在就能(3)。",
+			"你真的想能够(3)吗？",
+			"如果你能(3)，那又如何？"
 		]],
 		["* i don't *", [
-			"Don't you really (2) ?",
-			"Why don't you (2) ?",
-			"Do you wish to be able to (2) ?",
-			"Does that trouble you ?"
+			"你真的不(2)吗？",
+			"你为什么不(2)？",
+			"你希望能够(2)吗？",
+			"这让你困扰吗？"
 		]],
 		["* i feel *", [
-			"Tell me more about such feelings.",
-			"Do you often feel (2) ?",
-			"Do you enjoy feeling (2) ?",
-			"Of what does feeling (2) remind you ?"
+			"告诉我更多关于这种感觉的事情。",
+			"你经常感到(2)吗？",
+			"你喜欢感到(2)吗？",
+			"感到(2)让你想起了什么？"
 		]],
 		["* i * you *", [
-			"Perhaps in your fantasies we (2) each other.",
-			"Do you wish to (2) me ?",
-			"You seem to need to (2) me.",
-			"Do you (2) anyone else ?"
+			"也许在你的幻想中，我们(2)彼此。",
+			"你想(2)我吗？",
+			"你好像很需要(2)我。",
+			"你(2)别人吗？"
 		]],
 		["*", [
-			"You say (1) ?",
-			"Can you elaborate on that ?",
-			"Do you say (1) for some special reason ?",
-			"That's quite interesting."
+			"你说(1)？",
+			"你能详细说说吗？",
+			"你为什么说(1)？",
+			"这很有趣。"
 		]]
 	]],
 	["you", 0, [
@@ -392,97 +393,97 @@ exports.elizaKeywords = [
 			"goto alike"
 		]],
 		["* you are *", [
-			"What makes you think I am (2) ?",
-			"Does it please you to believe I am (2) ?",
-			"Do you sometimes wish you were (2) ?",
-			"Perhaps you would like to be (2)."
+			"你为什么认为我是(2)？",
+			"你相信我是(2)让你高兴吗？",
+			"你有时候希望你是(2)吗？",
+			"也许你想成为(2)。"
 		]],
 		["* you* me *", [
-			"Why do you think I (2) you ?",
-			"You like to think I (2) you -- don't you ?",
-			"What makes you think I (2) you ?",
-			"Really, I (2) you ?",
-			"Do you wish to believe I (2) you ?",
-			"Suppose I did (2) you -- what would that mean ?",
-			"Does someone else believe I (2) you ?"
+			"你为什么认为我(2)你？",
+			"你喜欢认为我(2)你--对吗？",
+			"是什么让你觉得我(2)你？",
+			"真的，我(2)你？",
+			"你希望相信我(2)你吗？",
+			"假如我(2)你--那意味着什么？",
+			"有别人也认为我(2)你吗？"
 		]],
 		["* you *", [
-			"We were discussing you -- not me.",
-			"Oh, I (2) ?",
-			"You're not really talking about me -- are you ?",
-			"What are your feelings now ?"
+			"我们在讨论你--不是我。",
+			"哦，我(2)？",
+			"你不是真的在谈论我--对吗？",
+			"你现在的感受是什么？"
 		]]
 	]],
 	["yes", 0, [
 		["*", [
-			"You seem to be quite positive.",
-			"You are sure.",
-			"I see.",
-			"I understand."
+			"你看起来很肯定。",
+			"你很确定。",
+			"我明白了。",
+			"我理解了。"
 		]]
 	]],
 	["no", 0, [
 		["* no one *", [
-			"Are you sure, no one (2) ?",
-			"Surely someone (2) .",
-			"Can you think of anyone at all ?",
-			"Are you thinking of a very special person ?",
-			"Who, may I ask ?",
-			"You have a particular person in mind, don't you ?",
-			"Who do you think you are talking about ?"
+			"你确定，没有人(2)吗？",
+			"肯定有人(2)。",
+			"你能想到任何人吗？",
+			"你在想一个很特别的人吗？",
+			"谁，我可以问吗？",
+			"你心里有一个特定的人，对吧？",
+			"你觉得你在谈论谁？"
 		]],
 		["*", [
-			"Are you saying no just to be negative?",
-			"You are being a bit negative.",
-			"Why not ?",
-			"Why 'no' ?"
+			"你是不是说不只是为了否定？",
+			"你有点消极。",
+			"为什么不呢？",
+			"为什么'不'？"
 		]]
 	]],
 	["my", 2, [
 		["$ * my *", [
-			"Does that have anything to do with the fact that your (2) ?",
-			"Lets discuss further why your (2).",
-			"Earlier you said your (2).",
-			"But your (2)."
+			"这和你的(2)有关吗？",
+			"让我们进一步讨论你的(2)。",
+			"你之前说过你的(2)。",
+			"但是你的(2)。"
 		]],
 		["* my* @family *", [
-			"Tell me more about your family.",
-			"Who else in your family (4) ?",
-			"Your (3) ?",
-			"What else comes to your mind when you think of your (3) ?"
+			"告诉我更多关于你的家庭。",
+			"你家里还有谁(4)？",
+			"你的(3)？",
+			"当你想到你的(3)时，你还想到了什么？"
 		]],
 		["* my *", [
-			"Your (2) ?",
-			"Why do you say your (2) ?",
-			"Does that suggest anything else which belongs to you ?",
-			"Is it important to you that your (2) ?"
+			"你的(2)？",
+			"你为什么说你的(2)？",
+			"这是否暗示了你还有其他属于你的东西？",
+			"你的(2)对你很重要吗？"
 		]]
 	]],
 	["can", 0, [
 		["* can you *", [
-			"You believe I can (2) don't you ?",
+			"你相信我能(2)吗？",
 			"goto what",
-			"You want me to be able to (2).",
-			"Perhaps you would like to be able to (2) yourself."
+			"你希望我能(2)。",
+			"也许你想自己能(2)。"
 		]],
 		["* can i *", [
-			"Whether or not you can (2) depends on you more than on me.",
-			"Do you want to be able to (2) ?",
-			"Perhaps you don't want to (2).",
+			"你能不能(2)取决于你自己，而不是我。",
+			"你想能(2)吗？",
+			"也许你不想(2)。",
 			"goto what"
 		]]
 	]],
 	["what", 0, [
 		["*", [
-			"Why do you ask ?",
-			"Does that question interest you ?",
-			"What is it you really want to know ?",
-			"Are such questions much on your mind ?",
-			"What answer would please you most ?",
-			"What do you think ?",
-			"What comes to mind when you ask that ?",
-			"Have you asked such questions before ?",
-			"Have you asked anyone else ?"
+			"你为什么问？",
+			"这个问题对你有兴趣吗？",
+			"你真正想知道的是什么？",
+			"这样的问题经常在你的脑海里吗？",
+			"什么答案会让你最满意？",
+			"你怎么想？",
+			"你问这个问题时，你想到了什么？",
+			"你以前问过这样的问题吗？",
+			"你问过别人吗？"
 		]]
 	]],
 	["who", 0, [
@@ -507,25 +508,25 @@ exports.elizaKeywords = [
 	]],
 	["because", 0, [
 		["*", [
-			"Is that the real reason ?",
-			"Don't any other reasons come to mind ?",
-			"Does that reason seem to explain anything else ?",
-			"What other reasons might there be ?"
+			"那是真正的原因吗？",
+			"你没有想到其他的原因吗？",
+			"那个原因能解释其他的事情吗？",
+			"还有什么可能的原因？"
 		]]
 	]],
 	["why", 0, [
 		["* why don't you *", [
-			"Do you believe I don't (2) ?",
-			"Perhaps I will (2) in good time.",
-			"Should you (2) yourself ?",
-			"You want me to (2) ?",
+			"你相信我不(2)吗？",
+			"也许我会在适当的时候(2)。",
+			"你应该(2)自己吗？",
+			"你想让我(2)吗？",
 			"goto what"
 		]],
 		["* why can't i *", [
-			"Do you think you should be able to (2) ?",
-			"Do you want to be able to (2) ?",
-			"Do you believe this will help you to (2) ?",
-			"Have you any idea why you can't (2) ?",
+			"你觉得你应该能(2)吗？",
+			"你想能(2)吗？",
+			"你相信这会帮助你(2)吗？",
+			"你有什么想法，为什么你不能(2)？",
 			"goto what"
 		]],
 		["*", [
@@ -534,15 +535,15 @@ exports.elizaKeywords = [
 	]],
 	["everyone", 2, [
 		["* @everyone *", [
-			"Really, (2) ?",
-			"Surely not (2).",
-			"Can you think of anyone in particular ?",
-			"Who, for example?",
-			"Are you thinking of a very special person ?",
-			"Who, may I ask ?",
-			"Someone special perhaps ?",
-			"You have a particular person in mind, don't you ?",
-			"Who do you think you're talking about ?"
+			"真的，(2)？",
+			"肯定不是(2)。",
+			"你能想到任何特定的人吗？",
+			"比如说，谁？",
+			"你在想一个很特别的人吗？",
+			"谁，我可以问吗？",
+			"也许是一个特别的人？",
+			"你心里有一个特定的人，对吧？",
+			"你觉得你在谈论谁？"
 		]]
 	]],
 	["everybody", 2, [
@@ -562,22 +563,22 @@ exports.elizaKeywords = [
 	]],
 	["always", 1, [
 		["*", [
-			"Can you think of a specific example ?",
-			"When ?",
-			"What incident are you thinking of ?",
-			"Really, always ?"
+			"你能想到一个具体的例子吗？",
+			"什么时候？",
+			"你在想什么事情？",
+			"真的，总是这样？"
 		]]
 	]],
 	["alike", 10, [
 		["*", [
-			"In what way ?",
-			"What resemblence do you see ?",
-			"What does that similarity suggest to you ?",
-			"What other connections do you see ?",
-			"What do you suppose that resemblence means ?",
-			"What is the connection, do you suppose ?",
-			"Could there really be some connection ?",
-			"How ?"
+			"在哪些方面？",
+			"你看到了什么相似之处？",
+			"这种相似性对你意味着什么？",
+			"你还看到了什么其他的联系？",
+			"你认为这种相似性意味着什么？",
+			"你认为有什么联系吗？",
+			"真的有某种联系吗？",
+			"怎么样？"
 		]]
 	]],
 	["like", 10, [
@@ -587,18 +588,19 @@ exports.elizaKeywords = [
 	]],
 	["different", 0, [
 		["*", [
-			"How is it different ?",
-			"What differences do you see ?",
-			"What does that difference suggest to you ?",
-			"What other distinctions do you see ?",
-			"What do you suppose that disparity means ?",
-			"Could there be some connection, do you suppose ?",
-			"How ?"
+			"它有什么不同？",
+			"你看到了什么差异？",
+			"这种差异对你意味着什么？",
+			"你还看到了什么其他的区别？",
+			"你认为这种差异意味着什么？",
+			"你认为有可能有某种联系吗？",
+			"怎么样？"
 		]]
 	]]
 
 ];
 
+// ** TODO: Missing translation
 // regexp/replacement pairs to be performed as final cleanings
 // here: cleanings for multiple bots talking to each other
 exports.elizaPostTransforms = [
